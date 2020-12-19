2426d32401ae7d28bbbd40a5edd96561bdcbeb9a
adrian <adrian-sw@aporter.org>
2020-12-19 02:18:30 -0500

    +14.

--- 14.py
+++ 14.py
@@ -7,6 +7,30 @@ mem[7] = 101
 mem[8] = 0
 '''.strip().splitlines()
 
+
+data='''
+mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1
+'''.strip().splitlines()
+
+data='''
+mask = 100X000X100X00XX1010X0001X11XX100110
+mem[33470] = 43619
+mem[17642] = 12960
+mem[54949] = 1594
+mem[25705] = 17992
+mem[28651] = 47662
+'''.strip().splitlines()
+
+data='''
+mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1
+'''.strip().splitlines()
+
 data='''
 mask = 100X000X100X00XX1010X0001X11XX100110
 mem[33470] = 43619
@@ -581,23 +605,6 @@ mem[9726] = 42312
 mem[14459] = 15520
 '''.strip().splitlines()
 
-data='''
-mask = 000000000000000000000000000000X1001X
-mem[42] = 100
-mask = 00000000000000000000000000000000X0XX
-mem[26] = 1
-'''.strip().splitlines()
-
-data='''
-mask = 100X000X100X00XX1010X0001X11XX100110
-mem[33470] = 43619
-mem[17642] = 12960
-mem[54949] = 1594
-mem[25705] = 17992
-mem[28651] = 47662
-'''.strip().splitlines()
-
-
 
 mem=[0 for i in range(65536)]
 ones=(1<<36)-1
@@ -620,6 +627,25 @@ print(sum(mem))
 mem={}
 ones=(1<<36)-1
 
+def getFloatMask(id, floatMask):
+	position = 0
+	outputFloatMask = 0
+	while id > 0:
+		if id&1:
+			floatMaskPosition=0
+			currentFloatMask = floatMask
+			currentPosition = 0
+			while currentFloatMask>0:
+				if currentFloatMask&1:
+					if currentPosition == position:
+						outputFloatMask += 1<<floatMaskPosition
+					currentPosition+=1
+				currentFloatMask>>=1
+				floatMaskPosition += 1
+		id>>=1
+		position+=1
+	return outputFloatMask
+
 
 for line in data:
 	var, value = line.split(' = ')
@@ -630,32 +656,18 @@ for line in data:
 
 	else:
 		index = int(var.replace('mem[', '').replace(']', ''))
-		index |= orMask
-		length = sum(int(v) for v in bin(floatMask)[2:])
-		for id in range(0, 1<<length):
-			floatOr = 0
-			floatAnd = 0
-			currentFloat = floatMask
-			while currentFloat > 0:
-				floatOr<<=1
-				floatAnd<<=1
-				if currentFloat&(1<<35) != 0:
-					if id&1!=0:
-						floatOr+=1
-					else:
-						floatAnd+=1
-					id>>=1
-				currentFloat=(currentFloat<<1) & ones
+		current = floatMask
+		floatMaskOnes=0
+		while current > 0:
+			if current & 1:
+				floatMaskOnes+=1
+			current>>=1
+		for id in range(0, 1<<floatMaskOnes):
+			outputIndex = ((index | orMask) & (ones-floatMask)) | getFloatMask(id, floatMask)
+			#print(index, orMask, getFloatMask(id, floatMask), outputIndex)
+			mem[outputIndex] = int(value)
 
-			floatAnd = ones - floatAnd
-			print(bin(index), bin(floatAnd), bin(floatOr), index & floatAnd | floatOr)
-			mem[str(index & floatAnd | floatOr)] = int(value)
-		print()
 
 print(sum(mem.values()))
-
-#4996090846172 is wrong (too high?)
-#4794460460592 is too low
-
 
 
