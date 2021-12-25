87f877575f81b53b76eccdc9733e57f555d5fc70
adrian <adrian-sw@aporter.org>
2021-12-25 00:16:57 -0500

    +25.

--- template.py
+++ template.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+import math, re, sys, itertools
+from sortedcontainers import SortedList #pip3 install sortedcontainers
+from collections import defaultdict, deque, Counter
 
 data1='''
 '''.strip().splitlines()
@@ -9,11 +12,10 @@ data2='''
 data=data1
 
 #data = [int(line) for line in data]
+#data = [[int(column) for column in re.findall('-?[\d]+', line)] for line in data]
 #data = [[int(column) for column in line] for line in data]
 #data = [[column for column in line] for line in data]
 #data = [[int(column) for column in line.split(',')] for line in data]
 
 #for line in data:
-
-
 
