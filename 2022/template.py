e5edbc1be874aeeb9fdbbe56cb50314b37d5cec8
adrian <adrian-sw@aporter.org>
2022-12-01 00:06:49 -0500

    +1.

---
+++ 2022/template.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import math, re, sys, itertools
+from sortedcontainers import SortedList #pip3 install sortedcontainers
+from collections import defaultdict, deque, Counter
+
+data1='''
+'''.strip().splitlines()
+data2='''
+'''.strip().splitlines()
+
+data=data1
+
+#data = [int(line) for line in data]
+#data = [[int(column) for column in re.findall('-?[\d]+', line)] for line in data]
+#data = [[int(column) for column in line] for line in data]
+#data = [[column for column in line] for line in data]
+#data = [[int(column) for column in line.split(',')] for line in data]
+
+#for line in data:
+
