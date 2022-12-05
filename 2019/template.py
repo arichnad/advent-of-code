f9b0163b81b644c1ee6f96be31b67b09a2595e66
adrian <adrian-sw@aporter.org>
2022-12-04 23:44:10 -0500

    minor:  2019 template.

--- 2019/template.py
+++ 2019/template.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 
+import math, re, sys, itertools
+#from sortedcontainers import SortedList #pip3 install sortedcontainers
+from collections import defaultdict, deque, Counter
 
-data='''
+data1='''
 '''.strip().splitlines()
-data='''
+data2='''
 '''.strip().splitlines()
 
-#data=[int(d) for d in data[0].split(',')]
+data=data1
+
+#data = [int(line) for line in data]
+#data = [[int(column) for column in re.findall('-?[\d]+', line)] for line in data]
+#data = [[int(column) for column in line] for line in data]
+#data = [[column for column in line] for line in data]
+#data = [[int(column) for column in line.split(',')] for line in data]
 
 #for line in data:
-
-
-
-
 
