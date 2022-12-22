46542198db985e3975e2944b34c60990ded201f2
adrian <adrian-sw@aporter.org>
2022-12-22 02:10:29 -0500

    +22.

--- 2022/template.py
+++ 2022/template.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 import math, re, sys, itertools, functools, copy
-from sortedcontainers import SortedList #python3 -mpip install install sortedcontainers #SortedList('bat') + 'cat'
-from astar.search import AStar #python3 -mpip install python-astar #print(AStar([[0]]).search((0,0), (0,0)))
-from collections import defaultdict, deque, Counter
+#from sortedcontainers import SortedList #python3 -mpip install install sortedcontainers #SortedList('bat') + 'cat'
+#from astar.search import AStar #python3 -mpip install python-astar #print(AStar([[0]]).search((0,0), (0,0)))
+#from collections import defaultdict, deque, Counter
+#from z3 import * #python3 -mpip install install z3 z3-solver #s = Solver(); x = Int('x'); y = Int('y'); s.add(x < 10); print(s, s.check(), s.model() if s.check()==sat else '')
 
 data1='''
 
