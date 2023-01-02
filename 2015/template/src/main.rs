f8d3c20a849bea07246e34b07c1fc9bdbbbba27b
adrian <adrian-sw@aporter.org>
2023-01-01 20:27:11 -0500

    +1.

---
+++ 2015/template/src/main.rs
@@ -0,0 +1,29 @@
+
+#![allow(dead_code, unused)]
+
+use std::str::Lines;
+use regex::Regex;
+
+fn main() {
+
+	let data1 : Lines = "
+
+".trim_matches('\n').lines();
+	let data2 : Lines = "
+
+".trim_matches('\n').lines();
+
+
+	let data = data1;
+
+	let parsed = data;
+	//let parsed: Vec<Vec<i32>> = data.map(|line| Regex::new(r"-?\d+").unwrap().find_iter(line).map(|mat| mat.as_str().parse::<i32>().expect("need an integer")).collect()).collect();
+	//let parsed: Vec<i32> = data.map(|line| line.parse::<i32>().expect("need an integer")).collect();
+	//let parsed: Vec<Vec<i32>> = data.map(|line| line.split_whitespace().map(|column| column.parse::<i32>().expect("need an integer")).collect()).collect();
+	//let parsed: Vec<Vec<i32>> = data.map(|line| line.split(',').map(|column| column.parse::<i32>().expect("need an integer")).collect()).collect();
+
+	for line in parsed {
+
+	}
+}
+
