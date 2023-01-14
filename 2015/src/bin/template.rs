bcfeb047cd05511976d24e629e8f3f2da1a9da89
adrian <adrian-sw@aporter.org>
2023-01-14 11:55:02 -0500

    +12.

--- 2015/src/bin/template.rs
+++ 2015/src/bin/template.rs
@@ -6,7 +6,7 @@ use std::collections::HashSet;
 use std::str::Lines;
 
 use regex::Regex; //for back references:  use fancy_regex::Regex;
-use serde_json::to_string;
+use serde_json::{to_string, from_str, Value};
 
 //noinspection DuplicatedCode
 fn main() {
@@ -19,14 +19,13 @@ fn main() {
 "#.trim_matches('\n').lines().map(ToString::to_string).collect::<Vec<String>>();
 
 
-	let unparsed = data1;
+	let mut data = data1;
 
-	let mut data = unparsed;
-	//let mut data: Vec<Vec<i32>> = unparsed.iter().map(|line| Regex::new(r"-?\d+").unwrap().find_iter(line).map(|mat| mat.as_str().parse::<i32>().expect("need an integer")).collect()).collect();
-	//let mut data: Vec<i32> = unparsed.iter().map(|line| line.parse::<i32>().expect("need an integer")).collect();
-	//let mut data: Vec<Vec<i32>> = unparsed.iter().map(|line| line.split_whitespace().map(|column| column.parse::<i32>().expect("need an integer")).collect()).collect();
-	//let mut data: Vec<Vec<i32>> = unparsed.iter().map(|line| line.split(',').map(|column| column.parse::<i32>().expect("need an integer")).collect()).collect();
-	//json: println!("{}", to_string(&data).unwrap());
+	//let mut data: Vec<Vec<i32>> = data.iter().map(|line| Regex::new(r"-?\d+").unwrap().find_iter(line).map(|mat| mat.as_str().parse::<i32>().expect("need an integer")).collect()).collect();
+	//let mut data: Vec<i32> = data.iter().map(|line| line.parse::<i32>().expect("need an integer")).collect();
+	//let mut data: Vec<Vec<i32>> = data.iter().map(|line| line.split_whitespace().map(|column| column.parse::<i32>().expect("need an integer")).collect()).collect();
+	//let mut data: Vec<Vec<i32>> = data.iter().map(|line| line.split(',').map(|column| column.parse::<i32>().expect("need an integer")).collect()).collect();
+	//json: println!("{} {}", to_string(&data).unwrap(), from_str::<Value>(&data[0]).unwrap());
 
 	for line in &data {
 
