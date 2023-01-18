ed4cb2602a3a6da3ed6e4c9a737e5b1b1e309d46
adrian <adrian-sw@aporter.org>
2023-01-17 22:18:03 -0500

    +18.

--- 2015/src/bin/template.rs
+++ 2015/src/bin/template.rs
@@ -24,9 +24,35 @@ fn main() {
 
 	//let mut data: Vec<Vec<i32>> = data.iter().map(|line| Regex::new(r"-?\d+").unwrap().find_iter(line).map(|mat| mat.as_str().parse::<i32>().expect("need an integer")).collect()).collect();
 	//let mut data: Vec<i32> = data.iter().map(|line| line.parse::<i32>().expect("need an integer")).collect();
-	//let mut data: Vec<Vec<i32>> = data.iter().map(|line| line.split_whitespace().map(|column| column.parse::<i32>().expect("need an integer")).collect()).collect();
 	//let mut data: Vec<Vec<i32>> = data.iter().map(|line| line.split(',').map(|column| column.parse::<i32>().expect("need an integer")).collect()).collect();
+	//let mut data: Vec<Vec<i32>> = data.iter().map(|line| line.chars().map(|column| column.to_digit(10).expect("need an integer") as i32).collect()).collect();
+	//let mut data: Vec<Vec<char>> = data.iter().map(|line| line.chars().collect()).collect();
 	//json: println!("{} {}", to_string(&data).unwrap(), from_str::<Value>(&data[0]).unwrap());
+
+	/*
+	let mut data: Vec<Vec<char>> = data.iter().map(|line| line.chars().collect()).collect();
+	let W = data[0].len();
+	let H = data.len();
+	for j in 0..H {
+		for i in 0..W {
+			let mut total = 0;
+			for dx in -1..2 {
+				for dy in -1..2 {
+					// if dx==0 && dy==0 {continue;}
+					if dx==0 && dy==0 || dx!=0 && dy!=0 {continue;}
+					let newX = i as i32 + dx;
+					let newY = j as i32 + dy;
+					if newX<0 || newY<0 || newX as usize>=W || newY as usize>=H {continue;}
+					let newX = newX as usize; let newY = newY as usize;
+				}
+			}
+		}
+	}
+	for j in 0..H {
+		for i in 0..W { print!("{}", data[j][i]); }
+		println!();
+	}
+	*/
 
 	for line in &data {
 
--- 2022/template.py
+++ 2022/template.py
@@ -10,15 +10,30 @@ data1='''
 
 '''.strip('\n').splitlines()
 data2='''
+
 '''.strip('\n').splitlines()
 
 data=data1
 
-#data = [int(line) for line in data]
 #data = [[int(column) for column in re.findall('-?[\d]+', line)] for line in data]
+#data = [int(line) for line in data]
 #data = [[int(column) for column in line] for line in data]
-#data = [[column for column in line] for line in data]
 #data = [[int(column) for column in line.split(',')] for line in data]
+#data = [[column for column in line] for line in data]
+
+#data = [[column for column in line] for line in data]
+#W,H=len(data[0]), len(data)
+#for j in range(H):
+#	for i in range(W):
+#		for dy in range(-1, 2):
+#			for dx in range(-1, 2):
+#				#if dx==0 and dy==0: continue
+#				if dx==0 and dy==0 or dx!=0 and dy!=0: continue
+#				
+#				newY,newX=j+dy,i+dx
+#				if newY<0 or newX<0 or newY>=H or newX>=W: continue
+#				
+#for line in data: print(''.join(line))
 
 #for line in data:
 
