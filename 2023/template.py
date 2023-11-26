f20204bbe6238af159ef6582f10c8f4114a72313
adrian <adrian-sw@aporter.org>
2023-11-26 11:29:22 -0500

    start up for 2023 using an ide, wow.

---
+++ 2023/template.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import math, re, sys, itertools, functools, copy, json
+#from sortedcontainers import SortedList #python3 -mpip install install sortedcontainers #SortedList('bat') + 'cat'
+#from astar.search import AStar #python3 -mpip install python-astar #print(AStar([[0]]).search((0,0), (0,0)))
+#from collections import defaultdict, deque, Counter
+#from z3 import * #python3 -mpip install install z3 z3-solver #s = Solver(); x = Int('x'); y = Int('y'); s.add(x < 10); print(s, s.check(), s.model() if s.check()==sat else '')
+
+data1='''
+
+'''.strip('\n').splitlines()
+data2='''
+
+'''.strip('\n').splitlines()
+
+data=data1
+
+#data = [int(line) for line in data]
+#data = [[int(column) for column in re.findall('-?[\d]+', line)] for line in data]
+#data = [[int(column) for column in line] for line in data]
+#data = [[int(column) for column in line.split(',')] for line in data]
+#data = [[column for column in line] for line in data]
+
+#data = [[column for column in line] for line in data]
+#W,H=len(data[0]), len(data)
+#for j in range(H):
+#	for i in range(W):
+#		for dy in range(-1, 2):
+#			for dx in range(-1, 2):
+#				#if dx==0 and dy==0: continue
+#				if dx==0 and dy==0 or dx!=0 and dy!=0: continue
+#
+#				newY,newX=j+dy,i+dx
+#				if newY<0 or newX<0 or newY>=H or newX>=W: continue
+#
+#for line in data: print(''.join(line))
+
+#for line in data:
+
