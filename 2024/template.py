2bb180f3a0519a087b5731fc7dfee4b181e13e04
adrian <adrian-sw@aporter.org>
2024-12-02 00:20:00 -0500

    +2.

--- 2024/template.py
+++ 2024/template.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+# noinspection PyUnresolvedReferences
 import math, re, sys, itertools, functools, copy, json, threading, numpy
 #sudo apt install python3-dev pypy3-dev python3-sortedcontainers python3-z3 python3-sympy python3-shapely python3-numpy
 sys.setrecursionlimit(100000)
