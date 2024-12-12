401e8d1d8788e1ae42b8c1338e0c6795c6e50d60
adrian <adrian-sw@aporter.org>
2024-12-11 21:21:06 -0500

    +8.

---
+++ 2019/8.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+
+# noinspection PyUnresolvedReferences
+import math, re, sys, itertools, functools, copy, json, threading, numpy
+# settings -> project -> python interpreter -> add new -> /usr/bin/pypy3 -> add new -> virtual environment .venv based on pypy3
+# OR sudo apt install python3-dev pypy3-dev python3-sortedcontainers python3-z3 python3-sympy python3-shapely python3-numpy
+sys.setrecursionlimit(100000)
+# from sortedcontainers import SortedList #python3 -mpip install install sortedcontainers #SortedList('bat') + 'cat'
+# from astar import AStar #python3 -mpip install astar #see astarExample.py
+# from collections import defaultdict, deque, Counter
+# from z3 import * #python3 -mpip install install z3 z3-solver # s = Solver(); x = Real('x'); y = Real('y'); s.add([x <= 10, x >= 10]); print(s); print(s.check()) # if s.check()==z3.sat: print(int(str(s.model()[x]))) #don't use Int or Ints:  they are very slow
+# import lmfit #sudo apt install python3-dev pypy3-dev libopenblas-dev gfortran && python3 -mpip install lmfit
+# from sympy import * #python3 -mpip install sympy # x,y=symbols('x y'); print(solve([x <= 10, x >= 10]))
+# from shapely import Polygon #print(Polygon([(0,0),(1,0),(1,1)]).area) #sudo apt install python3-dev pypy3-dev libgeos-dev && python3 -mpip install shapely
+
+data1='''
+0222112222120000
+'''.strip('\n').splitlines()
+data2='''
+
+'''.strip('\n').splitlines()
+
+# data=data1; W=2; H=2
+data=data2; W=25; H=6
+
+data = [int(line) for line in data[0]]
+#data = [[int(column) for column in re.findall('-?[\\d]+', line)] for line in data]
+#data = [[int(column) for column in line] for line in data]
+#data = [[int(column) for column in line.split(',')] for line in data]
+#data = [[column for column in line] for line in data]
+#data = [threading.Thread(target=lambda line: print(line), args=(line)) for line in data] #line.start() line.join()
+# W,H=len(data[0]),len(data)
+
+# p=0
+# out=[]
+# while p<len(data):
+#     z,o,t=0,0,0
+#     for j in range(H):
+#         for i in range(W):
+#             if data[p]==0:
+#                 z+=1
+#             elif data[p]==1:
+#                 o+=1
+#             elif data[p]==2:
+#                 t+=1
+#             p+=1
+#     out.append((z, o*t))
+# out=sorted(out)
+# print(out[0][1])
+
+p=0
+out=[[None for i in range(W)] for j in range(H)]
+while p<len(data):
+    for j in range(H):
+        for i in range(W):
+            if data[p]==0 or data[p]==1:
+                if out[j][i] is None:
+                    out[j][i] = data[p]
+            p+=1
+for line in out: print(''.join(['#' if i==1 else ' ' for i in line]))
+
+#dir = (dir+4)%4
+#dx,dy = [(1,0),(0,1),(-1,0),(0,-1)][dir] #clockwise, starting right
+#dir = 1 if dy==1 else 3 if dx==0 else 0 if dx==1 else 2 #clockwise, starting right
+#dir = 'rdlu'.find(d.lower()) #clockwise, starting right
+#dir = ['right', 'down', 'left', 'up'].index(d.lower()) #clockwise, starting right
+#dir = '>v<^'.find(d.lower()) #clockwise, starting right
+
+#data = [[column for column in line] for line in data]
+#for j in range(H):
+#	for i in range(W):
+#		for dy in range(-1, 2):
+#			for dx in range(-1, 2):
+#				#if dx==0 and dy==0: continue
+#				if dx==0 and dy==0 or dx!=0 and dy!=0: continue
+#
+#				newY,newX=j+dy,i+dx
+#				if newY<0 or newX<0 or newY>=H or newX>=W: continue
+#
+#for line in data: print(''.join(line))
+
