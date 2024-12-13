e51a041b05024af2fee405e5d193a04f312c05f0
adrian <adrian-sw@aporter.org>
2024-12-13 01:28:29 -0500

    +13.

---
+++ 2024/13.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+
+# noinspection PyUnresolvedReferences
+import math, re, sys, itertools, functools, copy, json, threading, numpy # list(itertools.permutations(range(4), 4))
+# settings -> project -> python interpreter -> add new -> /usr/bin/pypy3 -> add new -> virtual environment .venv based on pypy3
+# OR sudo apt install python3-dev pypy3-dev python3-sortedcontainers python3-z3 python3-sympy python3-shapely python3-numpy
+sys.setrecursionlimit(100000)
+# from sortedcontainers import SortedList #python3 -mpip install install sortedcontainers #SortedList('bat') + 'cat'
+# from astar import AStar #python3 -mpip install astar #see astarExample.py
+# from collections import defaultdict, deque, Counter
+# from z3 import * #python3 -mpip install install z3 z3-solver # s = Solver(); x = Real('x'); y = Real('y'); s.add([x <= 10, x >= 10]); print(s); print(s.check()) # if s.check()==z3.sat: print(int(str(s.model()[x]))) #don't use Int or Ints:  they are very slow
+# import lmfit #sudo apt install python3-dev pypy3-dev libopenblas-dev gfortran && python3 -mpip install lmfit
+# from sympy import * #python3 -mpip install sympy # x,y=symbols('x y'); print(solve([x <= 10, x >= 10]))
+# from shapely import Polygon #print(Polygon([(0,0),(1,0),(1,1)]).area) #sudo apt install python3-dev pypy3-dev libgeos-dev && python3 -mpip install shapely
+
+data1='''
+Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279
+'''.strip('\n').splitlines()
+data2='''
+
+'''.strip('\n').splitlines()
+
+data=data2
+
+#data = [int(line) for line in data]
+data = [[int(column) for column in re.findall('-?[\\d]+', line)] for line in data]
+data.append([])
+#data = [[int(column) for column in line] for line in data]
+#data = [[int(column) for column in line.split(',')] for line in data]
+#data = [[column for column in line] for line in data]
+#data = [threading.Thread(target=lambda line: print(line), args=(line)) for line in data] #line.start() line.join()
+
+
+# (ax*a, ay*a) + (bx*b, by*b) = (x, y)
+
+answer=0
+for i in range(len(data)//4):
+	[ax,ay] = data[i*4+0]
+	[bx, by] = data[i * 4 + 1]
+	[prizeX, prizeY] = data[i * 4 + 2]
+	prizeX+=10000000000000;prizeY+=10000000000000
+
+	x = (by / bx * prizeX - prizeY) / (by / bx - ay / ax)
+	x=int(round(x))
+	if x // ax * ax + (prizeX - x) // bx * bx != prizeX:
+		continue
+	if x // ax * ay + (prizeX - x) // bx * by != prizeY:
+		continue
+	out = (x // ax) * 3 + (prizeX - x) // bx
+	answer+=out
+print(answer)
+
+
+#dir = (dir+4)%4
+#dx,dy = [(1,0),(0,1),(-1,0),(0,-1)][dir] #clockwise, starting right
+#dir = 1 if dy==1 else 3 if dx==0 else 0 if dx==1 else 2 #clockwise, starting right
+#dir = 'rdlu'.find(d.lower()) #clockwise, starting right
+#dir = ['right', 'down', 'left', 'up'].index(d.lower()) #clockwise, starting right
+#dir = '>v<^'.find(d.lower()) #clockwise, starting right
+
+#data = [[column for column in line] for line in data]
+#for j in range(H):
+#	for i in range(W):
+#		for dy in range(-1, 2):
+#			for dx in range(-1, 2):
+#				#if dx==0 and dy==0: continue
+#				if dx==0 and dy==0 or dx!=0 and dy!=0: continue
+#
+#				newY,newX=j+dy,i+dx
+#				if newY<0 or newX<0 or newY>=H or newX>=W: continue
+#
+#for line in data: print(''.join(line))
+
