9fd2d2f7837e94a6bb1d5500995d5216d10ad527
adrian <adrian-sw@aporter.org>
2025-12-04 00:06:02 -0500

    +4.

---
+++ 2025/4.py
@@ -0,0 +1,143 @@
+#!/usr/bin/python3
+
+# noinspection PyUnresolvedReferences
+import math, re, sys, itertools, functools, copy, json, threading, random, heapq, time # list(itertools.permutations(range(4), 4)); heapq.heappush(a, 3)
+# settings -> project -> python interpreter -> add new -> /usr/bin/pypy3 -> add new -> virtual environment .venv based on pypy3
+# OR sudo apt install python3-dev pypy3-dev python3-sortedcontainers python3-z3 python3-sympy python3-shapely python3-numpy
+sys.setrecursionlimit(100000)
+# from sortedcontainers import SortedList #python3 -mpip install install sortedcontainers #SortedList('bat') + 'cat'
+# from astar import AStar #python3 -mpip install astar #see astarExample.py
+# from collections import defaultdict, deque, Counter
+# from z3 import * #python3 -mpip install install z3 z3-solver # s = Solver(); x = Real('x'); y = Real('y'); s.add([x <= 10, x >= 10]); print(s); print(s.check()) # if s.check()==z3.sat: print(int(str(s.model()[x]))) #don't use Int or Ints:  they are very slow
+# import lmfit #sudo apt install python3-dev pypy3-dev libopenblas-dev gfortran && python3 -mpip install lmfit
+# from sympy import * #python3 -mpip install sympy # x,y=symbols('x y'); print(solve([x <= 10, x >= 10]))
+# from shapely import Polygon #print(Polygon([(0,0),(1,0),(1,1)]).area) #sudo apt install python3-dev pypy3-dev libgeos-dev && python3 -mpip install shapely
+# from testtemplate import *
+
+data1='''
+'''.strip('\n').splitlines()
+data2='''
+'''.strip('\n').splitlines()
+
+data=data2
+
+# data = [int(line) for line in data]; H=len(data)
+# data = [[int(column) for column in re.findall('-?\d+', line)] for line in data]; W,H=len(data[0]),len(data)
+# data = [[int(column) for column in line] for line in data]; W,H=len(data[0]),len(data)
+# data = [[int(column) for column in line.split(',')] for line in data]; W,H=len(data[0]),len(data)
+data = [[column for column in line] for line in data]; W,H=len(data[0]),len(data)
+# python threads are not real:  thread=threading.Thread(target=lambda line: print(line), args=(line)); thread.start(); thread.join() # does not run in parallel on separate cores
+
+answer=0
+going=True
+# for line in data:
+while going:
+	going=False
+	for j in range(H):
+		for i in range(W):
+			a=0
+			for dy in range(-1, 2):
+				for dx in range(-1, 2):
+					if dx==0 and dy==0: continue
+					# if dx==0 and dy==0 or dx!=0 and dy!=0: continue
+					newY,newX=j+dy,i+dx
+					if newY<0 or newX<0 or newY>=H or newX>=W: continue
+					a+=1 if data[newY][newX]=='@' else 0
+			if a<4 and data[j][i]=='@':
+				going=True
+				answer+=1
+				data[j][i]='.'
+				# print(j,i)
+print(answer)
+
+# dir = (dir+4)%4
+# dx,dy = [(1,0),(0,1),(-1,0),(0,-1)][dir] # clockwise, starting right
+# dir = 1 if dy==1 else 3 if dx==0 else 0 if dx==1 else 2 # clockwise, starting right
+# dir = 'rdlu'.find(d.lower()) # clockwise, starting right
+# dir = ['right', 'down', 'left', 'up'].index(d.lower()) # clockwise, starting right
+# dir = '>v<^'.find(d.lower()) # clockwise, starting right
+
+# data = [[column for column in line] for line in data]
+# for j in range(H):
+# 	for i in range(W):
+# 		for dy in range(-1, 2):
+# 			for dx in range(-1, 2):
+# 				# if dx==0 and dy==0: continue
+# 				if dx==0 and dy==0 or dx!=0 and dy!=0: continue
+# 
+# 				newY,newX=j+dy,i+dx
+# 				if newY<0 or newX<0 or newY>=H or newX>=W: continue
+# 
+# for line in data: print(''.join(line))
+
+# bfs simple queue
+# for j in range(H):
+# 	for i in range(W):
+# 		if data[j][i]=='S': startY,startX=j,i
+# 		if data[j][i]=='E': endY,endX=j,i
+# positions = [(0, startY, startX)]
+# costs={}
+# while len(positions) > 0:
+# 	# print(len(positions))
+# 	newPositions = []
+# 	for (cost, y, x) in sorted(positions): # remove sorted here if it's not needed
+# 		if y<0 or y>=H or x<0 or x>=W: continue
+# 		if data[y][x] == '#': continue
+# 		if y==endY and x==endX:
+# 			print(cost)
+# 			newPositions=[]
+# 			break
+# 		if (y, x) in costs and cost >= costs[(y, x)]: continue # change >= here to > if you need to analyze ties
+# 		costs[(y, x)] = cost
+# 
+# 		for dy in range(-1, 2):
+# 			for dx in range(-1, 2):
+# 				# if dx==0 and dy==0: continue
+# 				if dx==0 and dy==0 or dx!=0 and dy!=0: continue
+# 				newPositions.append((cost + 1, y+dy, x+dx))
+# 	positions=newPositions
+
+
+# bfs (priority queue)
+# for j in range(H):
+# 	for i in range(W):
+# 		if data[j][i]=='S': startY,startX=j,i
+# 		if data[j][i]=='E': endY,endX=j,i
+# positions = [(0, startY, startX)]
+# costs={}
+# while len(positions) > 0:
+# 	(cost, y, x) = heapq.heappop(positions)
+# 	if y<0 or y>=H or x<0 or x>=W: continue
+# 	if data[y][x]=='#': continue
+# 	if (y, x) in costs and cost >= costs[(y, x)]: continue
+# 	costs[(y, x)] = cost
+# 	
+# 	for dy in range(-1, 2):
+# 		for dx in range(-1, 2):
+# 			# if dx==0 and dy==0: continue
+# 			if dx==0 and dy==0 or dx!=0 and dy!=0: continue
+# 			if (y+dy, x+dx) in costs and cost+1 >= costs[(y+dy, x+dx)]: continue
+# 			heapq.heappush(positions, (cost+1, y+dy, x+dx))
+# print(costs[(endY, endX)])
+
+# flood fill does NOT work when want a breadth first (minimum cost, that sort of thing)
+# for j in range(H):
+# 	for i in range(W):
+# 		if data[j][i]=='S': startY,startX=j,i
+# 		if data[j][i]=='E': endY,endX=j,i
+# visited=set()
+# def ff(y, x):
+# 	if y<0 or x<0 or y>=H or x>=W: return 0
+# 	if data[y][x] == '#' or (y, x) in visited: return 0
+# 	visited.add((y, x))
+# 
+# 	answer=1
+# 	for dy in range(-1, 2):
+# 		for dx in range(-1, 2):
+# 			# if dx==0 and dy==0: continue
+# 			if dx==0 and dy==0 or dx!=0 and dy!=0: continue
+# 			answer+=ff(y+dy, x+dx)
+# 	return answer
+# 
+# print(ff(startY, startX))
+
