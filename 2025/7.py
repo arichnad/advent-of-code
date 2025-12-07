33d7b3e74542a210095ef040545c193ef1e3e478
adrian <adrian-sw@aporter.org>
2025-12-07 00:35:52 -0500

    +7.

--- 2025/7.py
+++ 2025/7.py
@@ -3,41 +3,67 @@
 # noinspection PyUnresolvedReferences
 import math, re, sys, itertools, functools, copy, json, threading, random, heapq, time # list(itertools.permutations(range(4), 4)); heapq.heappush(a, 3)
 # settings -> project -> python interpreter -> add new -> /usr/bin/pypy3 -> add new -> virtual environment .venv based on pypy3
-# .venv/bin/pip3 install sortedcontainers astar z3 z3-solver lmfit sympy shapely
-# for lmfit:  sudo apt install python3-dev pypy3-dev libopenblas-dev gfortran
-# for shapely:  sudo apt install python3-dev pypy3-dev libgeos-dev
 # OR sudo apt install python3-dev pypy3-dev python3-sortedcontainers python3-z3 python3-sympy python3-shapely python3-numpy
 sys.setrecursionlimit(100000)
-from sortedcontainers import SortedList #SortedList('bat') + 'cat'
-from astar import AStar #see astarExample.py
-from collections import defaultdict, deque, Counter
-from z3 import * # s = Solver(); x = Real('x'); y = Real('y'); s.add([x <= 10, x >= 10]); print(s); print(s.check()) # if s.check()==z3.sat: print(int(str(s.model()[x]))) #don't use Int or Ints:  they are very slow
-import lmfit
-from sympy import * # x,y=symbols('x y'); print(solve([x <= 10, x >= 10]))
-from shapely import Polygon #print(Polygon([(0,0),(1,0),(1,1)]).area)
-from testtemplate import *
+# from sortedcontainers import SortedList #python3 -mpip install sortedcontainers #SortedList('bat') + 'cat'
+# from astar import AStar #python3 -mpip install astar #see astarExample.py
+# from collections import defaultdict, deque, Counter
+# from z3 import * # s = Solver(); x = Real('x'); y = Real('y'); s.add([x <= 10, x >= 10]); print(s); print(s.check()) # if s.check()==z3.sat: print(int(str(s.model()[x]))) #don't use Int or Ints:  they are very slow
+# import lmfit #sudo apt install python3-dev pypy3-dev libopenblas-dev gfortran && python3 -mpip install lmfit
+# from sympy import * #python3 -mpip install sympy # x,y=symbols('x y'); print(solve([x <= 10, x >= 10]))
+# from shapely import Polygon #print(Polygon([(0,0),(1,0),(1,1)]).area) #sudo apt install python3-dev pypy3-dev libgeos-dev && python3 -mpip install shapely
+# from testtemplate import *
 
 data1='''
-
 '''.strip('\n').splitlines()
 data2='''
-
 '''.strip('\n').splitlines()
 
-data=data1
+data=data2
 
 # data = [int(line) for line in data]; H=len(data)
 # data = [[int(column) for column in re.findall('-?\d+', line)] for line in data]; W,H=len(data[0]),len(data)
 # data = [[int(column) for column in line] for line in data]; W,H=len(data[0]),len(data)
 # data = [[int(column) for column in line.split(',')] for line in data]; W,H=len(data[0]),len(data)
-# data = [[column for column in line] for line in data]; W,H=len(data[0]),len(data)
+data = [[column for column in line] for line in data]; W,H=len(data[0]),len(data)
 # python threads are not real:  thread=threading.Thread(target=lambda line: print(line), args=(line)); thread.start(); thread.join() # does not run in parallel on separate cores
 
+# xs=set()
+# for j in range(H):
+# 	for i in range(W):
+# 		if data[j][i]=='S': startY,startX=j,i
+# 		if data[j][i]=='E': endY,endX=j,i
+# xs.add(startX)
+# answer=0
+# for j in range(H):
+# 	for x in xs.copy():
+# 		if data[j][x]=='^':
+# 			answer+=1
+# 			xs.remove(x)
+# 			xs.add(x+1)
+# 			xs.add(x-1)
+# print(answer)
+xs={}
+for j in range(H):
+	for i in range(W):
+		if data[j][i]=='S': startY,startX=j,i
+		if data[j][i]=='E': endY,endX=j,i
+xs[startX]=1
+for j in range(H):
+	newRow={}
+	for x in xs.keys():
+		if data[j][x]=='^':
+			for v in (x-1, x+1):
+				if v not in newRow:
+					newRow[v]=0
+				newRow[v]+=xs[x]
+		else:
+			if x not in newRow:
+				newRow[x]=0
+			newRow[x]+=xs[x]
+	xs=newRow
 
-# for line in data:
-
-
-
+print(sum([x for x in xs.values()]))
 # dir = (dir+4)%4
 # dx,dy = [(1,0),(0,1),(-1,0),(0,-1)][dir] # clockwise, starting right
 # dir = 1 if dy==1 else 3 if dx==0 else 0 if dx==1 else 2 # clockwise, starting right
