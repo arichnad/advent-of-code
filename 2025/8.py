37c6a8e6daa387d1d3deac6dc35e6ae946f443c0
adrian <adrian-sw@aporter.org>
2025-12-08 00:48:20 -0500

    +8.

--- 2025/8.py
+++ 2025/8.py
@@ -18,25 +18,95 @@ sys.setrecursionlimit(100000)
 # from testtemplate import *
 
 data1='''
-
 '''.strip('\n').splitlines()
 data2='''
-
 '''.strip('\n').splitlines()
 
-data=data1
+data=data2
 
 # data = [int(line) for line in data]; H=len(data)
-# data = [[int(column) for column in re.findall('-?\d+', line)] for line in data]; W,H=len(data[0]),len(data)
+data = [[int(column) for column in re.findall('-?\d+', line)] for line in data]; W,H=len(data[0]),len(data)
 # data = [[int(column) for column in line] for line in data]; W,H=len(data[0]),len(data)
 # data = [[int(column) for column in line.split(',')] for line in data]; W,H=len(data[0]),len(data)
 # data = [[column for column in line] for line in data]; W,H=len(data[0]),len(data)
 # python threads are not real:  thread=threading.Thread(target=lambda line: print(line), args=(line)); thread.start(); thread.join() # does not run in parallel on separate cores
 
+dist=[]
+for a in range(H):
+	for b in range(a+1, H):
+		# if a in s and b in s and s[a]==s[b]: continue
+		cur=(data[a][0]-data[b][0])**2+(data[a][1]-data[b][1])**2+(data[a][2]-data[b][2])**2
+		dist.append((cur, a, b))
 
-# for line in data:
+s={}
+t=[[i] for i in range(H)]
+for i in range(H):
+	s[i]=i
+
+dist=sorted(dist, reverse=True)
+print(dist[-1])
+while True:
+	(m,ma,mb) = dist.pop()
+	print(len(dist), m, data[ma][0], data[mb][0], s)
+	sma,smb=s[ma],s[mb]
+	if sma==smb: continue
+	# print(sma, smb)
+	for x in t[smb]: t[sma].append(x)
+	t[smb]=[]
+	s[mb]=s[ma]
+	for x in t[sma]: s[x]=sma
+	last=(m,data[ma][0],data[mb][0])
+	if len(t[sma])==H: break
+
+print(last[1]*last[2])
 
 
+
+# s={}
+# d=set()
+# for changes in range(1000):
+# 	m=math.inf
+# 	for a in range(H):
+# 		for b in range(H):
+# 			if a==b: continue
+# 			if (a,b) in d: continue
+# 			# if a in s and b in s and s[a]==s[b]: continue
+# 			cur=(data[a][0]-data[b][0])**2+(data[a][1]-data[b][1])**2+(data[a][2]-data[b][2])**2
+# 			if cur<m:
+# 				m,ma,mb=cur,a,b
+# 	d.add((ma,mb)); d.add((mb,ma))
+# 	# print(data[ma], data[mb])
+# 	if ma not in s:
+# 		s[ma]=set()
+# 	if mb not in s:
+# 		s[mb]=set()
+# 	s[ma].add(ma)
+# 	s[mb].add(mb)
+# 	changes=True
+# 	while changes:
+# 		changes=False
+# 		for x in s[mb].copy():
+# 			for y in s[ma].copy():
+# 				if y not in s[x]:
+# 					s[x].add(y)
+# 					changes=True
+# 				if x not in s[y]:
+# 					s[y].add(x)
+# 					changes=True
+#
+# print(s)
+# v=set()
+# answer=[]
+# for i in s.values():
+# 	aa=tuple(sorted(list(i)))
+# 	if aa in v: continue
+# 	v.add(aa)
+# 	print(len(i), aa)
+# 	answer.append(len(i))
+# answerTotal=1
+# for a in sorted(answer, reverse=True)[:3]:
+# 	answerTotal*=a
+# print(answerTotal)
 
 # dir = (dir+4)%4
 # dx,dy = [(1,0),(0,1),(-1,0),(0,-1)][dir] # clockwise, starting right
